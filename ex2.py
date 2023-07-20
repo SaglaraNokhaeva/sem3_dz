223e6ac4a88e10d7ea1cf84ba57650bf4fa7b4f5
Saglara <nohaevass@yandex.ru>
2023-07-21 00:44:15 +0300

    1

--- ex2.py
+++ ex2.py
@@ -1,2 +1,11 @@
 # Дан список повторяющихся элементов. Вернуть список с дублирующимися элементами.
 # В результирующем списке не должно быть дубликатов.
+
+my_list = input("Введите список повторяющихся элементов через пробел: ").split()
+duplicate_list = []
+for i in list(set(my_list)):
+    if my_list.count(i) >= 2:
+        for j in range(my_list.count(i)):
+            duplicate_list.append(i)
+print("Список без дубликатов: ", *set(my_list))
+print("Список с дублирующимися элементами: ", *duplicate_list)
--- ex3.py
+++ ex3.py
@@ -1,3 +1,39 @@
 # В большой текстовой строке подсчитать количество встречаемых слов и вернуть 10 самых частых.
 # Не учитывать знаки препинания и регистр символов. За основу возьмите любую статью из википедии или
 # из документации к языку.
+
+import wikipedia
+
+DUPLICATE = 10
+
+my_str = wikipedia.summary("Python (programming language)").lower()
+print("Исходная строка: ", my_str)
+my_list = my_str.replace(';',' ').replace(',',' ').replace('.',' ').replace('"',' ').replace(':',' ').replace('!',' ').split()
+print("Разбили строку: ", my_list)
+new_list = []
+for i in my_list:
+    if i not in new_list:
+        new_list.append(i)
+print("Список слов без повторов: ", new_list)
+rez = {}
+for i in new_list:
+    count = my_list.count(i)
+    rez[i] = count
+print("Словарь слов с количествами повторений: ", rez)
+sorted_dict = {}
+sorted_keys = sorted(rez, key=rez.get, reverse=True)  # [1, 3, 2]
+
+for _ in sorted_keys:
+    sorted_dict[_] = rez[_]
+
+print("Отсортировали словарь: ", sorted_dict)
+print("10 самых частых слов: ", *list(sorted_dict.items())[:DUPLICATE])
+
+
+
+
+
+# print(wikipedia.page("Python (programming language)").content)
+
+
+
