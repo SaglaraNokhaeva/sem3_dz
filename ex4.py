cd110e86a53f57542ef4ccf725b2d1dc92f0d4d9
Saglara <nohaevass@yandex.ru>
2023-07-21 01:11:05 +0300

    1

--- ex4.py
+++ ex4.py
@@ -2,3 +2,16 @@
 # Определите какие вещи влезут в рюкзак передав его максимальную грузоподъёмность.
 # Достаточно вернуть один допустимый вариант.
 
+my_dict = {'water': 2000, 'food': 1000, 'dishes': 300, 'first_aid_kit': 400, 'clothes': 500, 'flashlight': 300}
+max_weight = int(input("Введите максимальную грузоподъёмность в граммах: "))
+summ_weight = 0
+print("Берём в рюкзак: ")
+for key, value in my_dict.items():
+    if (summ_weight + value) <= max_weight:
+        summ_weight += value
+        print(key, " - Итого:", summ_weight)
+
+    else:
+        quit()
+
+
